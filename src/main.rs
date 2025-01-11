4dccc49250c5dd913433a86f6e3b4cd6d6af9d69
Mandar Ghade <GX2H912@gmail.com>
2025-01-10 23:36:16 -0500

    feat: implements `CompoundModifier` trait for ref counted `Compound`s

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 //use thiserror::Error;
 
 use std::{
-    borrow::BorrowMut,
+    borrow::{Borrow, BorrowMut},
     cell::{BorrowMutError, RefCell},
+    collections::{HashMap, HashSet},
+    fmt::Debug,
     ops::Index,
+    path::Iter,
     rc::Rc,
 };
 
@@ -16,7 +19,7 @@ enum Chirality {
 }
 
 #[allow(unused)]
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct Element {
     number: u8,
 }
@@ -28,7 +31,7 @@ impl Element {
 }
 
 #[allow(unused)]
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct Atom {
     element: Element,
     neutrons: u8,
@@ -56,11 +59,13 @@ enum ParsingError {
 
 #[derive(Error, Clone, Debug, Display)]
 enum CompoundUsageError {
-    PushToSubstituents(String),
+    SubstituentPushingError(String),
+    CompoundPushingError(String),
+    ParsingError(ParsingError),
 }
 
 #[allow(unused)]
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum Particle {
     Atom(Atom),
     Molecule(Vec<Atom>),
@@ -76,94 +81,149 @@ impl Particle {
     }
 }
 
+struct AtomCounter {
+    atoms: HashMap<u8, u8>,
+}
+
+impl ToString for AtomCounter {
+    fn to_string(&self) -> String {
+        todo!()
+    }
+}
+
+// utilize compound iterable class
+impl AtomCounter {
+    fn from_compound(cmp: &Compound) -> Self {
+        todo!()
+    }
+}
+
 #[allow(unused)]
-#[derive(Clone, Debug)]
+#[derive(Clone, PartialEq)]
 struct Compound {
-    // this is sort of like an iterator
+    // Atom chain. Composite data-structure of individual `Compound` fragments,
+    // abstraction of an Organic Compound in chemistry, but heavily convoluted
     center: Atom,
     subst: Vec<Particle>,                    // substituents
     side_chains: Vec<Rc<RefCell<Compound>>>, // previous compounds
 }
 
+impl Debug for Compound {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Compound")
+            .field("center", &self.center)
+            .field("subst", &self.subst)
+            .field("side_chains_count", &self.side_chains.len())
+            .finish()
+    }
+}
+
 impl Compound {
-    fn new(
-        center: Atom,
-        subst: Vec<Particle>,
-        side_chains: Vec<Rc<RefCell<Compound>>>,
-    ) -> Result<Compound, ParsingError> {
-        Ok(Compound {
-            center,
-            subst,
-            side_chains,
-        })
-        .map_err(ParsingError::CompoundParsingError)
-    }
-
-    fn as_box(
-        center: Atom,
-        subst: Vec<Particle>,
-        side_chains: Vec<Rc<RefCell<Compound>>>,
-    ) -> Result<Box<Compound>, ParsingError> {
-        Ok(Box::new(Compound {
-            center,
-            subst,
-            side_chains,
-        }))
-        .map_err(ParsingError::CompoundParsingError)
-    }
-
-    fn as_rc(
-        center: Atom,
-        subst: Vec<Particle>,
-        side_chains: Vec<Rc<RefCell<Compound>>>,
-    ) -> Result<Rc<RefCell<Compound>>, ParsingError> {
-        Ok(Rc::new(RefCell::new(Compound {
-            center,
-            subst,
-            side_chains,
-        })))
-        .map_err(ParsingError::CompoundParsingError)
-    }
-
-    fn as_rc_with_center(center: Atom) -> Result<Rc<RefCell<Compound>>, ParsingError> {
-        Ok(Rc::new(RefCell::new(Compound {
+    // Creates a `Compound` with initial center `Atom`
+    fn new(center: Atom) -> Self {
+        Self {
             center,
             subst: vec![],
             side_chains: vec![],
-        })))
-        .map_err(ParsingError::CompoundParsingError)
+        }
+    }
+
+    fn with_center(center: Atom) -> Self {
+        Self {
+            center,
+            subst: vec![],
+            side_chains: vec![],
+        }
+    }
+
+    fn as_rc_with_center(center: Atom) -> Rc<RefCell<Compound>> {
+        Self::with_center(center).rc()
+    }
+
+    fn condensed_formula(&self) -> String {
+        AtomCounter::from_compound(self).to_string()
     }
 }
 
-fn push_subst(cmp: Rc<RefCell<Compound>>, p: Particle) -> Result<(), BorrowMutError> {
-    (*cmp).try_borrow_mut()?.subst.push(p);
-    Ok(())
+trait ToRc {
+    fn rc(self) -> Rc<RefCell<Self>>;
 }
 
-fn push_side_chain(
-    cmp: Rc<RefCell<Compound>>,
-    chain: Rc<RefCell<Compound>>,
-) -> Result<(), BorrowMutError> {
-    (*cmp).try_borrow_mut()?.side_chains.push(chain);
-    Ok(())
+impl ToRc for Compound {
+    // Converts Compound to `Rc` so operations can be done on it
+    fn rc(self) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(self))
+    }
+}
+
+trait CompoundModifier: Sized {
+    // size of `Self` must be known at compile time
+    type Err;
+    fn add_substituent(&self, p: Particle) -> Result<Self, Self::Err>;
+    fn add_side_chain(&self, chain: Rc<RefCell<Compound>>) -> Result<Self, Self::Err>;
+}
+
+// Iterator of Compounds
+struct Compounds {
+    data: HashMap<Rc<RefCell<Compound>>, Vec<Rc<RefCell<Compound>>>>,
+    // HASHMAP!!!!!!!!!
+}
+
+// trait CompoundIterable {
+//     type Err;
+//     fn next_chain(&self) -> Result<Rc<RefCell<Compound>>, Self::Err>;
+//     fn next_substituent(&self) -> Result<&Particle, Self::Err>;
+// }
+
+impl CompoundModifier for Rc<RefCell<Compound>> {
+    type Err = CompoundUsageError;
+
+    fn add_substituent(&self, p: Particle) -> Result<Self, Self::Err> {
+        (*self)
+            .try_borrow_mut()
+            .map_err(|err| CompoundUsageError::SubstituentPushingError(err.to_string()))?
+            .subst
+            .push(p);
+        Ok(Rc::clone(self))
+    }
+    /// Adds a side chain to a `Compound`
+    /// (Extends a `Compound` using an existing chain)
+    ///
+    /// # Arguments
+    /// * `chain` - side_chain which `Compound` is directly attached to now.
+    ///
+    /// Returns
+    ///
+    /// The resulting compound once `chain`  been pushed to `Self`.
+    fn add_side_chain(&self, chain: Rc<RefCell<Compound>>) -> Result<Self, Self::Err> {
+        let same_chain = self.as_ptr() == chain.as_ptr();
+        let borrowed = (*self).try_borrow(); // immut
+        let vec_contains_chain =
+            borrowed.is_ok_and(|b| b.side_chains.iter().any(|c| c.as_ptr() == chain.as_ptr()));
+        if !same_chain && !vec_contains_chain {
+            (*self)
+                .try_borrow_mut()
+                .map_err(|e| CompoundUsageError::CompoundPushingError(e.to_string()))?
+                .side_chains
+                .push(Rc::clone(&chain)); // mut
+            chain.add_side_chain(Rc::clone(self))?; // preserves bi-directionality of compounds
+        }
+        Ok(Rc::clone(self))
+    }
+}
+
+fn test_compound() -> Result<Rc<RefCell<Compound>>, CompoundUsageError> {
+    let compound = Compound::with_center(Atom::new(3))
+        .rc()
+        .add_substituent(Particle::new_atom(Atom::new(18)))?
+        .add_substituent(Particle::new_molecule(vec![Atom::new(7), Atom::new(7)]))?;
+    let compound2 = Compound::as_rc_with_center(Atom::new(4)).add_side_chain(Rc::clone(&compound));
+    // Self referential side chain appending doesn't work
+    // Ex:
+    // cmp.add_side_chain(Rc::clone(&cmp))?;
+    Ok(compound)
 }
 
 fn main() {
-    let cmp = Compound::as_rc_with_center(Atom::new(3)).expect("Compound was expected");
-    {
-        let _ = push_subst(Rc::clone(&cmp), Particle::new_atom(Atom::new(18)));
-        let _ = push_subst(
-            Rc::clone(&cmp),
-            Particle::new_molecule(vec![Atom::new(7), Atom::new(7)]),
-        );
-    }
-    let cmp2 = Compound::as_rc_with_center(Atom::new(4)).expect("Compound was expected");
-    let cmp3 = Compound::as_rc_with_center(Atom::new(5)).expect("Compound was expected");
-    let _ = push_side_chain(Rc::clone(&cmp), Rc::clone(&cmp2));
-    let _ = push_side_chain(Rc::clone(&cmp), Rc::clone(&cmp3));
-    let _ = push_side_chain(Rc::clone(&cmp2), Rc::clone(&cmp));
-    {
-        let _ = push_subst(Rc::clone(&cmp2), Particle::new_atom(Atom::new(255)));
-    }
-    dbg!((*cmp).borrow_mut());
+    dbg!(test_compound());
 }
